c3201c4360bdbd87f209fd1a9c8bcd09c7e6184f
devonkennedy <128164883+devonkennedy@users.noreply.github.com>
2026-01-31 21:52:47 +0000

    Adding catsino and proximity interactions

---
+++ Ethel's Adventure 5/Assets/Scripts/ProximityInteractions.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.InputSystem; 
+using UnityEngine.Events;
+
+public class UniversalInteraction : MonoBehaviour
+{
+    [Header("UI Setup")]
+    public GameObject interactableUI; 
+    public float proximity = 3f;
+
+    [Header("The Interaction")]
+    public UnityEvent onInteract; 
+
+    private Transform player;
+
+    void Start()
+    {
+        //Finds player location
+        GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
+        if (playerObj != null) player = playerObj.transform;
+        
+        //Sets UI to off.
+        if (interactableUI != null) interactableUI.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (player == null || interactableUI == null) return;
+
+        //Finds proximity and sees if player is close enough
+        float dist = Vector2.Distance(transform.position, player.position);
+        bool isCloseEnough = dist < proximity;
+
+        interactableUI.SetActive(isCloseEnough);
+
+        // Check for Spacebar press
+        if (isCloseEnough && Keyboard.current != null && Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            // Run custom function
+            onInteract.Invoke(); 
+        }
+    }
+}
